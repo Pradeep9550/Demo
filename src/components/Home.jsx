27a2a654a1c7c5ac85ab6d9f37315ffab9184d80
Pradeep9550 <kapil95559@gmail.com>
2024-07-09 11:33:20 +0530

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,7 +32,6 @@ const Home = () => {
   
   return (
     <>
-      <h1>hello</h1>
       <div className='container'>
         <div className='search'>
           <input type='text' placeholder='Enter Dishe' onChange={handleInput}></input>
